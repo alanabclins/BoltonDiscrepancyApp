80de5f7b099d9aa4f7e646037b89c873658b1a0d
Alana Lins <alanacamaralins@gmail.com>
2023-08-21 17:37:20 -0300

    ajusts

--- Main.py
+++ Main.py
@@ -13,7 +13,7 @@ class Bolton (App):
         self.window = GridLayout ()
         with box.canvas.before:
             Color(1, 1, 1, 1) 
-            self.rect = Rectangle(size=(100000,100000))
+            self.rect = Rectangle(size=(10000,10000))
         self.window.cols = 1
         #add widgets to window
         self.window.add_widget(Image(source="Logo.png", size_hint_y=None, height=200))
@@ -22,22 +22,23 @@ class Bolton (App):
         text_input_color = (0.87, 0.93, 0.93, 1)
         self.pergunta_md_superior = Label(text="Medida mésio-distal dos 12 dentes superiores", font_size=20, color=label_color)
         self.window.add_widget(self.pergunta_md_superior)
-        self.md_superior=TextInput(multiline=False,background_normal='', background_color=text_input_color)
+        self.md_superior=TextInput(multiline=False,background_normal='', background_color=text_input_color,halign='center')
+        self.md_superior.halign= 'center'
         self.window.add_widget(self.md_superior)
 
         self.pergunta_md_inferior = Label(text="Medida mésio-distal dos 12 dentes inferiores", font_size=20, color=label_color)
         self.window.add_widget(self.pergunta_md_inferior)
-        self.md_inferior = TextInput(multiline=False,background_normal='', background_color=text_input_color)
+        self.md_inferior = TextInput(multiline=False,background_normal='', background_color=text_input_color,halign='center')
         self.window.add_widget(self.md_inferior)
 
         self.pergunta_md_anterior_superior = Label(text="Medida mésio-distal dos 6 dentes anteriores superiores",font_size=20,  color=label_color)
         self.window.add_widget(self.pergunta_md_anterior_superior)
-        self.md_anterior_superior = TextInput(multiline=False,background_normal='', background_color=text_input_color)
+        self.md_anterior_superior = TextInput(multiline=False,background_normal='', background_color=text_input_color,halign='center')
         self.window.add_widget(self.md_anterior_superior)
 
         self.pergunta_md_anterior_inferior = Label(text="Medida mésio-distal dos 6 dentes anteriores inferiores", font_size=20, color=label_color)
         self.window.add_widget(self.pergunta_md_anterior_inferior)
-        self.md_anterior_inferior = TextInput(multiline=False,background_normal='', background_color=text_input_color)
+        self.md_anterior_inferior = TextInput(multiline=False,background_normal='', background_color=text_input_color,halign='center')
         self.window.add_widget(self.md_anterior_inferior)
 
         #calculate
@@ -66,30 +67,24 @@ class Bolton (App):
                 if( 89.39 <= bolton_discrepancy <= 93.21):
                     self.result_label.text = "Paciente não apresenta discrepância de Bolton total"
                 else:
-                    """
-                    X= 12 Maxila do paciente - (12 mandibula do paciente / 0,8939)
-                    Y= (12 Maxila do paciente / 0,8939) - mandibula do paciente
-                    Z=  12 mandibula do paciente - (12 maxila do paciente / 0,9321)
-                    W= (12 mandibula do paciente / 0,9321) - maxila do paciente
-                    """
-                    x=12*md_superior_value-(12*md_inferior_value/0.8939);
-                    y= (12*md_superior_value/0.8939) - md_inferior_value; 
-                    z=12*md_superior_value-(12*md_inferior_value/0.9321)
-                    w=(12*md_superior_value/0.0321) - md_inferior_value;
+                    
+                    x=md_superior_value-(md_inferior_value/0.913);
+                    y= (md_superior_value*0.913) - md_inferior_value; 
+                    z=md_superior_value-(md_inferior_value/0.772)
+                    w=(md_superior_value*0.772) - md_inferior_value;
                     if(bolton_discrepancy<89.39):  
                         self.result_label.text = f"O paciente apresenta discrepância de Bolton total, podendo ser interpretada como excesso de {x:.2f}mm no arco superior ou deficiência de {y:.2f}mm no arco inferior."
                     else:
                         self.result_label.text = f"O paciente apresenta discrepância de Bolton total, podendo ser interpretada como deficiência de {z:.2f}mm no arco superior ou excesso de {w:.2f}mm no arco inferior."
-                #calculate canines, dont consider yet
-            if 1+1==2:
+                #calculando dos dentes anteriores
                 bolton_discrepancy= (md_anterior_inferior_value/md_anterior_superior_value)*100;  
                 if( 75.55  <= bolton_discrepancy <= 78.85):
                     self.result_anterior_label.text = "Paciente não apresenta discrepância de Bolton Anterior"
                 else:
-                    x=12*md_anterior_superior_value-(12*md_anterior_inferior_value/0.7555);
-                    y= (12*md_anterior_superior_value/0.7555) - md_anterior_inferior_value; 
-                    z=12*md_anterior_superior_value-(12*md_anterior_inferior_value/0.7885)
-                    w=(12*md_anterior_superior_value/0.7885) - md_anterior_inferior_value;
+                    x=md_anterior_superior_value-(md_anterior_inferior_value/0.772);
+                    y= (md_anterior_superior_value/0.772) - md_anterior_inferior_value; 
+                    z=md_anterior_superior_value-(md_anterior_inferior_value/0.772)
+                    w=(md_anterior_superior_value/0.772) - md_anterior_inferior_value;
                     if(bolton_discrepancy<75.55):  
                         self.result_anterior_label.text = f"O paciente apresenta discrepância de Bolton anterior, podendo ser interpretada como excesso de {x:.2f}mm no arco superior ou deficiência de {y:.2f}mm no arco inferior."
                     else:
@@ -98,7 +93,7 @@ class Bolton (App):
         except ValueError:
             self.result_label.text = "Insira apenas números para as medidas."
         
-        return self.window
+       
 
 
 Bolton().run()
